ea4211e8d95d0f1155a2f5a1a23e1b2f8810d0fb
linh.huathi <linhhuacntt@gmail.com>
2020-10-22 13:19:50 +0700

    fix: Revert from callback to async/await

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const REDIRECT_URL = OAuth2Data.web.redirect_uris;
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
 
-app.get('/', (req, res) => {
+const  checkAuth =  (req, res) => {
     if (!authed) {
         // Generate an OAuth URL and redirect there
         const url = oAuth2Client.generateAuthUrl({
@@ -24,12 +24,12 @@ app.get('/', (req, res) => {
         const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
         gmail.users.labels.list({
             userId: 'me',
-        }, (err, res) => {
+        }, async (err, res) => {
             if (err) return console.log('The API returned an error: ' + err);
-            const labels = res.data.labels;
+            const labels = await res.data.labels;
             if (labels.length) {
                 console.log('Labels:');
-                labels.forEach((label) => {
+                labels.forEach( async (label) => {
                     console.log(`- ${label.name}`);
                 });
             } else {
@@ -38,9 +38,40 @@ app.get('/', (req, res) => {
         });
         res.send('Logged in')
     }
-})
+};
 
-app.get('/auth/google/callback', function (req, res) {
+ app.get('/', checkAuth);
+
+// app.get('/', (req, res) => {
+//     if (!authed) {
+//         // Generate an OAuth URL and redirect there
+//         const url = oAuth2Client.generateAuthUrl({
+//             access_type: 'offline',
+//             scope: 'https://www.googleapis.com/auth/gmail.readonly'
+//         });
+//         console.log(url)
+//         res.redirect(url);
+//     } else {
+//         const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+//         gmail.users.labels.list({
+//             userId: 'me',
+//         }, (err, res) => {
+//             if (err) return console.log('The API returned an error: ' + err);
+//             const labels = res.data.labels;
+//             if (labels.length) {
+//                 console.log('Labels:');
+//                 labels.forEach((label) => {
+//                     console.log(`- ${label.name}`);
+//                 });
+//             } else {
+//                 console.log('No labels found.');
+//             }
+//         });
+//         res.send('Logged in')
+//     }
+// })
+
+const lastRes = function (req, res) {
     const code = req.query.code
     if (code) {
         // Get an access token based on our OAuth code
@@ -56,7 +87,27 @@ app.get('/auth/google/callback', function (req, res) {
             }
         });
     }
-});
+}
+
+app.get('/auth/google/callback', lastRes);
+
+// app.get('/auth/google/callback', function (req, res) {
+//     const code = req.query.code
+//     if (code) {
+//         // Get an access token based on our OAuth code
+//         oAuth2Client.getToken(code, function (err, tokens) {
+//             if (err) {
+//                 console.log('Error authenticating')
+//                 console.log(err);
+//             } else {
+//                 console.log('Successfully authenticated');
+//                 oAuth2Client.setCredentials(tokens);
+//                 authed = true;
+//                 res.redirect('/')
+//             }
+//         });
+//     }
+// });
 
 const port = process.env.port || 5000
-app.listen(port, () => console.log(`Server running at ${port}`));
+app.listen( port, async () =>  await console.log(`Server running at ${port}`));
